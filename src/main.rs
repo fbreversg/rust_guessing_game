0d105813b8f0b15e0d7d059e24d98d662a1c6721
fbreversg <etiorum@gmail.com>
2022-02-15 16:47:46 +0100

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::io;
 
 fn main() {
-    
     println!("Guess the number");
     println!("Please input your guess.");
 
@@ -9,7 +8,6 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    
-    
+
     println!("You guessed: {}", guess);
-    }
+}
